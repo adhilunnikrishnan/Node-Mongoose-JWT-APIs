be8d3b306c3f5d820d7f45720fc5204d56655857
adhilunnikrishnan <adhilunnikrishnanofficial@gmail.com>
2025-10-28 14:08:27 +0530

    feat:authvalidation addded

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { loginUser, registerUser } from "../controllers/authController.js";
+import { validate } from "../middleware/validationMiddleware.js";
+import { registerValidation } from "../validations/authValidation.js";
 
 const authRoutes = express.Router({ mergeParams: true });
 
 // ✅ POST /api/register
-authRoutes.post("/register", registerUser);
+authRoutes.post("/register", validate(registerValidation), registerUser);
 
 authRoutes.post("/login", loginUser);
 
